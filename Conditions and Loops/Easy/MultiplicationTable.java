777a617fd5a1efedc3ee93046b90cdfcf049ca00
Aritra Chatterjee <aritrachatterjee83@gmail.com>
2023-06-28 11:34:44 +0530

    Conditions and Loops Solutions Added

---
+++ Conditions and Loops/Easy/MultiplicationTable.java
@@ -0,0 +1,16 @@
+////Write a program that prompts the user to input a positive integer. It should then print the multiplication table of that number.
+//package com.aritra;
+//import java.util.*;
+//public class MultiplicationTable {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int[] nums = {1,2,3,4,5};
+//        change(nums);
+//        System.out.println(Arrays.toString(nums));
+//    }
+//    static void change(int b,String c)
+//    {
+//
+//    }
+//    static void change(String b,int c)
+//}
