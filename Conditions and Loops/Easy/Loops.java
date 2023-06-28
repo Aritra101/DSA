b9e546e3e7bbb8f080ccc4e6fc55ad8b42eff676
Aritra Chatterjee <aritrachatterjee83@gmail.com>
2023-06-28 11:16:43 +0530

    Conditions and Loops Solutions Added

---
+++ Conditions and Loops/Easy/Loops.java
@@ -0,0 +1,40 @@
+package com.aritra;
+
+public class Loops {
+    /* For Loop
+    for(initialisation; condition; increment/decrement){
+        body of the loop
+    }
+    Example:
+    for(i=0;i<=5;i++){
+        System.out.println(i);
+        }
+     */
+
+    /* While Loop
+    while(condition){
+        body of the loop
+        increment/decrement
+    }
+        Example:
+    int i = 0
+    while(i<=5){
+        System.out.println(i);
+        i++;
+        }
+     */
+    /* Note - We should use for loop when we know how many times the loop will run( for example print first 10 numbers)
+              We should use while loop when we don't know how many times the loop will run( for example keep on printing till the user presses 'X') */
+
+    /* Do While Loop
+    do{
+    body
+    }while(condition);
+    Example:
+    int i = 1;
+    do{
+    System.out.println(i)
+    }while(i<=5);
+     */
+
+}
